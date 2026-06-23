a0d39fc50bd74bdfdedf1f9c5faa046ab4a1a142
manacommunityapp <manacommunityapp@gmail.com>
2026-06-23 09:36:43 +0530

    changes for port

--- src/build-info.d.ts
+++ src/build-info.d.ts
@@ -2,4 +2,4 @@
 declare const __APP_VERSION__: string;
 declare const __GIT_BRANCH__: string;
 declare const __GIT_COMMIT__: string;
-declare const __BUILD_TIME__: string;
+declare const __BUILD_TIME__: string; 
